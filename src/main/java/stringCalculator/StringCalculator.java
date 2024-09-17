fa56258ad59add5bb0b74d134e47ad7773993859
sue-23 <85052088+sue-23@users.noreply.github.com>
2024-09-17 22:03:08 +0900

    feat: 문자열 계산기 테스트 코드 작성 완료

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -1,5 +1,6 @@
 package stringCalculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -10,6 +11,52 @@ public class StringCalculator {
         String input = scanner.nextLine();
         String[] inputs = input.split(" ");
         return inputs;
+    }
+
+    public Object[] separateNumbersAndOperators(String[] expression) {
+        Integer[] numbers = new Integer[expression.length / 2 + 1];
+        String[] operators = new String[expression.length / 2];
+        int numIndex = 0;
+        int opIndex = 0;
+        for (int i = 0; i < expression.length; i++) {
+            if (i % 2 == 0) {
+                numbers[numIndex++] = Integer.parseInt(expression[i]);
+            }else {
+                operators[opIndex++] = expression[i];
+            }
+        }
+        return new Object[] {numbers, operators};
+    }
+
+    public int calculate(Object[] separateResult) {
+        Integer[] numbers = (Integer[]) separateResult[0];
+        String[] operators = (String[]) separateResult[1];
+        int result = numbers[0];
+
+        for (int i = 0; i < operators.length; i++) {
+
+            String operator = operators[i];
+            int number = numbers[i+1];
+
+            switch (operator) {
+                case "+":
+                    result += number;
+                    break;
+                case "-":
+                    result -= number;
+                    break;
+                case "*":
+                    result *= number;
+                    break;
+                case "/":
+                    result /= number;
+                    break;
+
+                default:
+            }
+        }
+        return result;
+
     }
 
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,12 +2,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import stringCalculator.StringCalculator;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class StringCalculatorTest extends IOTest{
+public class StringCalculatorTest extends IOTest {
 
     private StringCalculator stringCalculator;
 
@@ -25,7 +26,50 @@ public class StringCalculatorTest extends IOTest{
         String[] inputs = stringCalculator.inputExpression();
         String[] expected = {"1", "+", "2", "+", "3"};
 
-        assertArrayEquals(expected, inputs);
+        assertThat(inputs).isEqualTo(expected);
+
+    }
+
+
+    @DisplayName("문자열 배열내의 숫자와 연산자 분리")
+    @ParameterizedTest
+    @ValueSource(strings = {"1 + 2 + 3"})
+        // 공백으로 구분된 문자열 전달
+    void seperateNumbersAndOperator(String expression) {
+        String[] expressionArray = expression.split(" ");
+
+        Object[] result = stringCalculator.separateNumbersAndOperators(expressionArray);
+        Integer[] resultNumbers = (Integer[]) result[0];
+        String[] resultOperators = (String[]) result[1];
+
+        Integer[] expectedNumbers = {1, 2, 3};
+        String[] expectedOperators = {"+", "+"};
+
+        assertThat(resultNumbers).isEqualTo(expectedNumbers);
+        assertThat(resultOperators).isEqualTo(expectedOperators);
+
+
+    }
+
+    @DisplayName("실제 계산 처리")
+    @ParameterizedTest
+    @CsvSource({
+            "'1 + 2 + 3', 6",
+            "'10 - 5', 5",
+            "'4 * 2', 8",
+            "'20 / 4', 5",
+            "'2 + 2 * 2', 8",
+            "'1 + 2 * 3 / 3', 3"
+    })
+    void calculate(String expression,int expected) {
+        systemIn(expression);
+        String[] inputs = stringCalculator.inputExpression();
+        Object[] seperated = stringCalculator.separateNumbersAndOperators(inputs);
+
+        int result = stringCalculator.calculate(seperated);
+
+        assertThat(result).isEqualTo(expected);
+
 
     }
 
